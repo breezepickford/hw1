bd5c8153b2711415c08d8ce51dfadc4d5d4ada24
Breeze Pickford <bpickfor@usc.edu>
2024-01-17 09:35:58 -0800

    started brainstorm

---
+++ recursive_split.cpp
@@ -0,0 +1,34 @@
+#include split.h
+
+struct Node {
+    int value;
+    Node *next;
+}
+
+void split (Node*& in, Node*& odds, Node*& evens) {
+   
+   
+   if (next exists) {
+        if (head odd) {
+
+        }
+        if (head even) {
+
+        }
+        //recursive
+        //get next in
+   }
+    //set in to null
+    //odds point to head of linked list
+    //even points to head of linked list
+    return 0;
+}
+
+
+
+int main() {
+    //call recursive funct
+    split(headptr, odd, even);
+
+    return 0;
+}
